47d5bb65d3e67957cf045a48f36be3ab8868bfcf
riyadomf <riyad.omf@gmail.com>
2025-09-25 23:06:30 +0600

    add debugger

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ class Settings:
     Central configuration file to store application-wide settings.
     """
     DATABASE_URL: str = "sqlite:///./data/flippit.db"
-    DATA_DIR: str = "data"
+    DATA_DIR: str = "backend/data"
     SOLD_PROPERTIES_CSV: str = os.path.join(DATA_DIR, "sold_properties.csv")
     FOR_SALE_PROPERTIES_CSV: str = os.path.join(DATA_DIR, "for_sale_properties.csv")
     MODEL_PATH: str = "resale_model.pkl"
